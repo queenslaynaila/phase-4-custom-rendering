debb4e2c80d08cd22e30071c0430bc76268d93dc
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-11-23 02:05:55 +0300

    complete

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -1,7 +1,21 @@
 class Cheese < ApplicationRecord
-  
+
+  def index
+    cheese = Cheese.all
+    render json: cheeses, except: [:created_at, :updated_at]
+  end
+
   def summary
     "#{name}: $#{price}"
+  end
+
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: { error: 'Cheese not found' },status: :not_found
+    end
   end
 
 end
